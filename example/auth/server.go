0d58a782b981c808d1e6543a10e01e86e17a7e19
Bui Ngoc Minh <minhnora98@gmail.com>
2019-02-11 20:38:33 +0700

    ADD zip file

--- example/auth/server.go
+++ example/auth/server.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"archive/zip"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/sessions"
 	"github.com/haicaodac/goshopify"
@@ -16,6 +20,64 @@ var (
 	key   = []byte("super-secret-key")
 	store = sessions.NewCookieStore(key)
 )
+
+// ZipFiles compresses one or many files into a single zip archive file.
+// Param 1: filename is the output zip file's name.
+// Param 2: files is a list of files to add to the zip.
+func ZipFiles(filename string, files []string) error {
+
+	newZipFile, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer newZipFile.Close()
+
+	zipWriter := zip.NewWriter(newZipFile)
+	defer zipWriter.Close()
+
+	// Add files to zip
+	for _, file := range files {
+		if err = AddFileToZip(zipWriter, file); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func AddFileToZip(zipWriter *zip.Writer, filename string) error {
+
+	fileToZip, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer fileToZip.Close()
+
+	// Get the file information
+	info, err := fileToZip.Stat()
+	if err != nil {
+		return err
+	}
+
+	header, err := zip.FileInfoHeader(info)
+	if err != nil {
+		return err
+	}
+
+	// Using FileInfoHeader() above only uses the basename of the file. If we want
+	// to preserve the folder structure we can overwrite this with the full path.
+	header.Name = filename
+
+	// Change to deflate to gain better compression
+	// see http://golang.org/pkg/archive/zip/#pkg-constants
+	header.Method = zip.Deflate
+
+	writer, err := zipWriter.CreateHeader(header)
+	if err != nil {
+		return err
+	}
+	_, err = io.Copy(writer, fileToZip)
+	return err
+}
 
 func main() {
 	app = goshopify.New(goshopify.Config{
@@ -36,12 +98,6 @@ func main() {
 	http.HandleFunc("/dashboard", func(w http.ResponseWriter, r *http.Request) {
 		session, _ := store.Get(r, "cookie-name")
 
-		// Check if user is authenticated
-		if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
-			http.Error(w, "Forbidden", http.StatusForbidden)
-			return
-		}
-
 		shopName := r.URL.Query().Get("shop")
 		hMac := r.URL.Query().Get("hmac")
 
@@ -59,10 +115,59 @@ func main() {
 			session.Values["authenticated"] = true
 			session.Save(r, w)
 		} else {
-			// Home pages
+			// Check if user is authenticated
+			if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
+				http.Error(w, "Forbidden", http.StatusForbidden)
+				return
+			}
 		}
 		fmt.Println(shopName)
 		// fmt.Println(authURL)
+	})
+
+	http.HandleFunc("/archive-zip", func(w http.ResponseWriter, r *http.Request) {
+		if _, err := os.Stat("temp"); os.IsNotExist(err) {
+			os.Mkdir("temp", 0777)
+		}
+
+		html := []byte(`<html>
+		<head>
+			<title>Xin chao</title>
+		</head>
+		<body>
+			<h1>dsahkjdhasjk</h1>
+		</body>
+	</html>`)
+		err := ioutil.WriteFile("temp/html.liquid", html, 0644)
+		if err != nil {
+			panic(err)
+		}
+
+		css := []byte(`.h1{
+			color: red;
+		}`)
+		err = ioutil.WriteFile("temp/css.liquid", css, 0644)
+		if err != nil {
+			panic(err)
+		}
+
+		js := []byte(`<script>
+		alert("xin chao")
+	</script>`)
+		err = ioutil.WriteFile("temp/js.liquid", js, 0644)
+		if err != nil {
+			panic(err)
+		}
+
+		// Zip ..
+		files := []string{"temp/html.liquid", "temp/css.liquid", "temp/js.liquid"}
+		output := "temp/theme.zip"
+
+		if err := ZipFiles(output, files); err != nil {
+			panic(err)
+		}
+		fmt.Println("Zipped File:", output)
+
 	})
 
 	http.HandleFunc("/shopify/auth", func(w http.ResponseWriter, r *http.Request) {
