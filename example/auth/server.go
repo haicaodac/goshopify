17792d72b68c1c8156c72fe7814936d296c15e54
Bui Ngoc Minh <minhnora98@gmail.com>
2019-02-11 22:01:22 +0700

    UPDATE code

--- example/auth/server.go
+++ example/auth/server.go
@@ -23,47 +23,7 @@ var (
 	store = sessions.NewCookieStore(key)
 )
 
-func removeIndexFromSlice(s []string, i int) []string {
-	s[i] = s[len(s)-1]
-	return s[:len(s)-1]
-}
 
-func appendFiles(filename string, zipw *zip.Writer) error {
-	currentDir, err := os.Getwd()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	defer os.Chdir(currentDir)
-
-	dir, err := filepath.Abs(filename)
-	if err != nil {
-		log.Fatalf("Failed to open %s: %s", filename, err)
-	}
-
-	arr := strings.Split(dir, "/")
-	filename = arr[len(arr)-1]
-	arr = removeIndexFromSlice(arr, len(arr)-1)
-	dir = strings.Join(arr, "/")
-	os.Chdir(dir)
-
-	file, err := os.Open(filename)
-	if err != nil {
-		return fmt.Errorf("Failed to open %s: %s", filename, err)
-	}
-	defer file.Close()
-
-	wr, err := zipw.Create(filename)
-	if err != nil {
-		msg := "Failed to create entry for %s in zip file: %s"
-		return fmt.Errorf(msg, filename, err)
-	}
-
-	if _, err := io.Copy(wr, file); err != nil {
-		return fmt.Errorf("Failed to write %s to zip: %s", filename, err)
-	}
-	return nil
-}
 
 func main() {
 	app = goshopify.New(goshopify.Config{
@@ -144,35 +104,11 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-
-		flags := os.O_WRONLY | os.O_CREATE | os.O_TRUNC
 		zipName := "temp/theme.zip"
-		file, err := os.OpenFile(zipName, flags, 0644)
-		if err != nil {
-			log.Fatalf("Failed to open zip for writing: %s", err)
-		}
-		defer file.Close()
-
 		files := []string{"temp/html.liquid", "temp/css.liquid", "temp/js.liquid"}
-
-		zipw := zip.NewWriter(file)
-		defer zipw.Close()
-
-		for _, filename := range files {
-			if err := appendFiles(filename, zipw); err != nil {
-				log.Fatalf("Failed to add file %s to zip: %s", filename, err)
-			}
-
-			// Remove file after append to zip
-			// if err := os.Remove(filename); err != nil {
-			// 	log.Fatalf("Failed to remove file %s: %s", filename, err)
-			// }
+		if err := ZipFiles(files, zipName); err != nil {
+			log.Println("Failed to zip files")
 		}
-
-		// Move zip to temp
-		// if err := os.Rename(zipName, "temp/"+zipName); err != nil {
-		// 	log.Fatalf("Failed to move zip file %s: %s", zipName, err)
-		// }
 
 	})
 
--- ulti.go
+++ ulti.go
@@ -1,7 +1,12 @@
 package goshopify
 
 import (
+	"archive/zip"
 	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -27,4 +32,81 @@ func ShopShortName(name string) string {
 func ShopBaseURL(name string) string {
 	name = ShopFullName(name)
 	return fmt.Sprintf("https://%s", name)
+}
+
+// RemoveIndexFromSlice ..
+func RemoveIndexFromSlice(s []string, i int) []string {
+	s[i] = s[len(s)-1]
+	return s[:len(s)-1]
+}
+
+func appendFiles(filename string, zipw *zip.Writer) error {
+	currentDir, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer os.Chdir(currentDir)
+
+	dir, err := filepath.Abs(filename)
+	if err != nil {
+		log.Fatalf("Failed to open %s: %s", filename, err)
+	}
+
+	arr := strings.Split(dir, "/")
+	filename = arr[len(arr)-1]
+	arr = RemoveIndexFromSlice(arr, len(arr)-1)
+	dir = strings.Join(arr, "/")
+	os.Chdir(dir)
+
+	file, err := os.Open(filename)
+	if err != nil {
+		return fmt.Errorf("Failed to open %s: %s", filename, err)
+	}
+	defer file.Close()
+
+	wr, err := zipw.Create(filename)
+	if err != nil {
+		msg := "Failed to create entry for %s in zip file: %s"
+		return fmt.Errorf(msg, filename, err)
+	}
+
+	if _, err := io.Copy(wr, file); err != nil {
+		return fmt.Errorf("Failed to write %s to zip: %s", filename, err)
+	}
+	return nil
+}
+
+// ZipFiles ..
+func ZipFiles(files []string, zipName string) error {
+	flags := os.O_WRONLY | os.O_CREATE | os.O_TRUNC
+	file, err := os.OpenFile(zipName, flags, 0644)
+	if err != nil {
+		log.Fatalf("Failed to open zip for writing: %s", err)
+		return err
+	}
+	defer file.Close()
+
+	zipw := zip.NewWriter(file)
+	defer zipw.Close()
+
+	for _, filename := range files {
+		if err := appendFiles(filename, zipw); err != nil {
+			log.Fatalf("Failed to add file %s to zip: %s", filename, err)
+			return err
+		}
+
+		// Remove file after append to zip
+		// if err := os.Remove(filename); err != nil {
+		// 	log.Fatalf("Failed to remove file %s: %s", filename, err)
+		// }
+	}
+
+	// Move zip to temp
+	// if err := os.Rename(zipName, "temp/"+zipName); err != nil {
+	// 	log.Fatalf("Failed to move zip file %s: %s", zipName, err)
+	// }
+
+	return nil
+
 }
